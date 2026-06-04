f89419152965c3f80e5b75e7572c844c2ca715e8
Simi268 <simikumari571@gmail.com>
2026-06-05 00:03:34 +0530

    configure sqlite database for deployment

--- apps/api/app/db/database.py
+++ apps/api/app/db/database.py
@@ -5,9 +5,12 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "sqlite:///./medintel.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
